3f7bb95cb01ff4f71eaeb4d4fcc4fd9b88d6acd0
cypher20b <nuel.emma20@gmail.com>
2024-07-16 06:36:52 +0000

    feat: password strength method implementation

--- src/app/components/password-card/password-card.component.ts
+++ src/app/components/password-card/password-card.component.ts
@@ -3,6 +3,7 @@ import { FormsModule } from '@angular/forms';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import {ClipboardModule} from '@angular/cdk/clipboard';
 import { DataStateService } from '../../services/data-state.service';
 import {MatSliderModule} from '@angular/material/slider';
 
@@ -14,7 +15,8 @@ import {MatSliderModule} from '@angular/material/slider';
     FormsModule, 
     MatButtonModule, 
     MatIconModule,
-    MatSliderModule
+    MatSliderModule,
+    ClipboardModule
   ],
   templateUrl: './password-card.component.html',
   styleUrls: ['./password-card.component.scss','./password-card-mobile.component.scss']
@@ -25,15 +27,26 @@ export class PasswordCardComponent {
   numbers:number = 0
   symbols:number = 0
   strengthClass:string = ''
-  pwdStringData:string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+[]{}|;:,.<>?"
+  pwdStringData:string = ''
+  pwdStringUppercase:string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  pwdStringLowercase:string = "abcdefghijklmnopqrstuvwxyz"
+  pwdStringNumbers:string = "0123456789"
+  pwdStringSymbol:string = "!@#$%^&*()_+[]{}|;:,.<>?"
   constructor(public dataStateService: DataStateService){}
 
   update(){
-    console.log(this.dataStateService.pwdChecker);
-    this.dataStateService.pwdChecker.upperCase ? this.uppercase = 25 : this.uppercase=0;
-    this.dataStateService.pwdChecker.lowerCase ? this.lowercase = 25 : this.lowercase=0;
-    this.dataStateService.pwdChecker.number ? this.numbers = 25 : this.numbers=0;
-    this.dataStateService.pwdChecker.symbol ? this.symbols = 25 : this.symbols=0;
+    this.dataStateService.pwdChecker.upperCase ? 
+      this.pwdStringData += this.pwdStringUppercase : this.pwdStringData.replace(this.pwdStringUppercase, '');
+    this.dataStateService.pwdChecker.lowerCase ? 
+      this.pwdStringData += this.pwdStringLowercase : this.pwdStringData.replace(this.pwdStringLowercase, '');
+    this.dataStateService.pwdChecker.number ? 
+      this.pwdStringData += this.pwdStringNumbers : this.pwdStringData.replace(this.pwdStringNumbers, '');
+    this.dataStateService.pwdChecker.symbol ? 
+      this.pwdStringData += this.pwdStringLowercase : this.pwdStringData.replace(this.pwdStringSymbol, '');
+    // this.dataStateService.pwdChecker.upperCase ? this.uppercase = 25 : this.uppercase=0;
+    // this.dataStateService.pwdChecker.lowerCase ? this.lowercase = 25 : this.lowercase=0;
+    // this.dataStateService.pwdChecker.number ? this.numbers = 25 : this.numbers=0;
+    // this.dataStateService.pwdChecker.symbol ? this.symbols = 25 : this.symbols=0;
 
     this.dataStateService.pwdStrength = this.uppercase + this.lowercase + this.numbers + this.symbols;
 
@@ -49,6 +62,10 @@ export class PasswordCardComponent {
       // this.pwdStringData.replace(this.pwdStringData.charAt(index),'')
     }
     this.dataStateService.generatedPwd = this.dataStateService.generatedPwd.slice(0, this.dataStateService.pwdLength)
+    (/[abc]/).test() this.dataStateService.generatedPwd ? this.uppercase = 25 : this.uppercase=0;
+    this.dataStateService.pwdChecker.lowerCase ? this.lowercase = 25 : this.lowercase=0;
+    this.dataStateService.pwdChecker.number ? this.numbers = 25 : this.numbers=0;
+    this.dataStateService.pwdChecker.symbol ? this.symbols = 25 : this.symbols=0;
     console.log(this.dataStateService.generatedPwd)
   }
 }
